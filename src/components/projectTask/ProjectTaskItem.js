c8132aa0bccec4c55fc1ec7623b25263c99b0f62
diegosalas27 <dominicsc2hs@gmail.com>
2019-04-06 15:03:48 -0500

    Created ProjectTaskItem, AddProjectTask form and UpdateTask form

---
+++ src/components/projectTask/ProjectTaskItem.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+class ProjectTaskItem extends Component {
+    render() {
+        return (
+            <div className="card mb-1 bg-light">
+    
+                <div className="card-header text-primary">
+                    ID: id
+                </div>
+                <div className="card-body bg-light">
+                    <h5 className="card-title">summary</h5>
+                    <p className="card-text text-truncate ">
+                        acceptance
+                    </p>
+                    <p className="card-text text-truncate ">
+                        Fecha límite: fecha
+                    </p>
+                    <a href={`updateProjectTask`} className="btn btn-primary">
+                        View / Update
+                    </a>
+    
+                    <button className="btn btn-danger ml-4">
+                        Delete  
+                    </button>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default ProjectTaskItem;
