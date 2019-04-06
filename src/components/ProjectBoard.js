01e80cd166cd00d4fab285deac26e34599e68829
diegosalas27 <dominicsc2hs@gmail.com>
2019-04-06 16:01:47 -0500

    Added functionality to get all projecttasks

--- src/components/ProjectBoard.js
+++ src/components/ProjectBoard.js
@@ -1,50 +1,103 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import ProjectTaskItem from './projectTask/ProjectTaskItem';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { getBacklog } from '../actions/projectTaskActions';
 
 class ProjectBoard extends Component {
-    render() {
-        return (
-            <div className="container">
-                <Link to="/addProjectTask" className="btn btn-primary mb-3">
-                    <i className="fas fa-plus-circle"> Crear tarea</i>
-                </Link>
-                <br />
-                <hr />
-                <div>
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-md-4">
-                                <div className="card text-center mb-2">
-                                    <div className="card-header bg-secondary text-white">
-                                        <h3>TO DO</h3>
-                                    </div>
-                                </div>
-                                <ProjectTaskItem />
-                            </div>
-                            <div className="col-md-4">
-                                <div className="card text-center mb-2">
-                                    <div className="card-header bg-primary text-white">
-                                        <h3>In Progress</h3>
-                                    </div>
-                                </div>
-                                <ProjectTaskItem />
-                            </div>
-                            <div className="col-md-4">
-                                <div className="card text-center mb-2">
-                                    <div className="card-header bg-success text-white">
-                                        <h3>Done</h3>
-                                    </div>
-                                </div>
-                                <ProjectTaskItem />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            
-        );
+    componentDidMount() {
+      this.props.getBacklog();
     }
+  
+    render() {
+      const { project_tasks } = this.props.project_tasks;
+  
+      let BoardContent;
+      let todoItems = []
+      let inProgressItems = []
+      let doneItems = []
+  
+      const BoardAlgorithm = project_tasks => {
+          if(project_tasks.length) {
+              const tasks = project_tasks.map(project_task => (
+                  <ProjectTaskItem key={project_task.id} project_task={project_task} />
+              ));
+  
+              for(let i = 0; i < tasks.length; i++){
+                  if(tasks[i].props.project_task.status === 1) {
+                      todoItems.push(tasks[i])
+                  } else if (tasks[i].props.project_task.status === 2) {
+                      inProgressItems.push(tasks[i])
+                  } else {
+                      doneItems.push(tasks[i])
+                  }
+              }
+  
+              return (
+                  <div>
+                      <div className="container">
+                          <div className="row">
+                              <div className="col-md-4">
+                                  <div className="card text-center mb-2">
+                                      <div className="card-header bg-secondary text-white">
+                                          <h3>TO DO</h3>
+                                      </div>
+                                  </div>
+                                  {todoItems}
+                              </div>
+                              <div className="col-md-4">
+                                  <div className="card text-center mb-2">
+                                      <div className="card-header bg-primary text-white">
+                                          <h3>In Progress</h3>
+                                      </div>
+                                  </div>
+                                  {inProgressItems}
+                              </div>
+                              <div className="col-md-4">
+                                  <div className="card text-center mb-2">
+                                      <div className="card-header bg-success text-white">
+                                          <h3>Done</h3>
+                                      </div>
+                                  </div>
+                                  {doneItems}
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              );
+  
+          } else {
+              return(
+                  <div className="alert alert-info text-center" role="alert">
+                      No hay tareas en este proyecto.
+                  </div>
+              );
+          }
+      };
+  
+      BoardContent = BoardAlgorithm(project_tasks);
+  
+      return (
+          <div className="container">
+          <Link to="/addProjectTask" className="btn btn-primary mb-3">
+              <i className="fas fa-plus-circle"> Crear tarea</i>
+          </Link>
+          <br />
+          <hr />
+          {BoardContent}
+          </div>
+      );
+    }
+  }
+
+ProjectBoard.propTypes = {
+    getBacklog: PropTypes.func.isRequired,
+    project_tasks: PropTypes.object.isRequired
 }
 
-export default ProjectBoard;
+const mapStateToProps = state => ({
+    project_tasks: state.project_task
+});
+
+export default connect(mapStateToProps, {getBacklog}) (ProjectBoard);
